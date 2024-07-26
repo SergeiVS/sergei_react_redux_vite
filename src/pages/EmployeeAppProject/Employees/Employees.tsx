ce036aab016f6077a22411275ff0380fc05cd12b
ValeriaKornienko <valebog94@gmail.com>
2024-07-26 21:44:51 +0200

    global delete button

--- src/pages/EmployeeAppProject/Employees/Employees.tsx
+++ src/pages/EmployeeAppProject/Employees/Employees.tsx
@@ -1,29 +1,41 @@
-import { ReactNode, useContext } from "react";
+import { ReactNode, useContext } from "react"
 
-import Employees_Card from "pages/EmployeeAppProject/components/EmployeesСard/EmployeesCard";
+import Employees_Card from "../components/EmployeesCard/EmployeesCard"
+import Button from "components/Button/Button"
 
-import { EmployeeAppContext } from "../contexts/EmployeeAppContext";
-import { EmployeesWrapper } from "./styles";
-import { Employee } from "../Layout_Team_1/types";
-import { v4 } from "uuid";
+import { EmployeeAppContext } from "../contexts/EmployeeAppContext"
+import { PageWrapper, EmployeesWrapper, DeleteButtonControl } from "./styles"
+import { Employee } from "../Layout_Team_1/types"
+import { v4 } from "uuid"
 
 function Employees() {
-  const { employees } = useContext(EmployeeAppContext);
+  const { employees } = useContext(EmployeeAppContext)
 
   const getEmployeesCards = (employees: Employee[]): ReactNode[] =>
     employees.map((emlpoyeeObj: Employee) => {
       return (
+        <PageWrapper>
+          <EmployeesWrapper>
         <Employees_Card
           key={v4()}
           name={emlpoyeeObj.name}
           surName={emlpoyeeObj.surName}
           age={emlpoyeeObj.age}
           jobPosition={emlpoyeeObj.jobPosition}
-        />
-      );
-    });
+        /></EmployeesWrapper>
+        <DeleteButtonControl><Button name="Remove all employees" onClick={() => {}} disabled={!employees} isRed={true} /></DeleteButtonControl>
+         </PageWrapper>
+      )
+    })
 
-  return <EmployeesWrapper>{getEmployeesCards(employees)}</EmployeesWrapper>;
+  return (
+      <>
+      {getEmployeesCards(employees)}
+      </>
+    
+    
+   
+  )
 }
 
-export default Employees;
+export default Employees
